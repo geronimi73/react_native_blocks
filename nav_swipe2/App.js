5641b5c19d0b95e9f005b25e025f3966bafd39e5
geronimi73 <gerald.stampfel+geronimo@gmail.com>
2026-01-06 13:24:46 +0100

    Update App.js

--- nav_swipe2/App.js
+++ nav_swipe2/App.js
@@ -9,12 +9,21 @@ import { NavigationContainer } from '@react-navigation/native';
 function HomeScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button screen="Details">Go to Details</Button>
+      <Button screen="Details1">Go to Details</Button>
+      <Button screen="Details2">Go to Details</Button>
     </View>
   );
 }
 
-function DetailsScreen() {
+function Details1Screen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+function Details2Screen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
@@ -25,8 +34,24 @@ function DetailsScreen() {
 const RootStack = createNativeStackNavigator({
   initialRouteName: 'Home',
   screens: {
-    Home: HomeScreen,
-    Details: DetailsScreen,
+    Home: {
+      screen: HomeScreen,
+      options: {
+        title: "Home"
+      }
+    },
+    Details1: {
+      screen: Details1Screen,
+      options: {
+        title: "Details One"
+      }
+    },
+    Details2: {
+      screen: Details2Screen,
+      options: {
+        title: "Details Two"
+      }
+    },
   },
 });
 
